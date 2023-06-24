9b9b633d0a40ca7f051192beaee13cbb9f30e72f
Accnt <accentrager@gmail.com>
2023-06-23 23:32:44 -0400

    Adios dudping

--- src/main/java/com/stiggles/smp5/listeners/NetheriteUpgrade.java
+++ src/main/java/com/stiggles/smp5/listeners/NetheriteUpgrade.java
@@ -4,7 +4,9 @@ import com.stiggles.smp5.items.NetheriteQuestItems;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.inventory.PrepareSmithingEvent;
+import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.SmithingInventory;
 
@@ -17,6 +19,14 @@ public class NetheriteUpgrade implements Listener {
             return;
         } else if (sInv.contains(Material.NETHERITE_UPGRADE_SMITHING_TEMPLATE)){
             e.setResult(new ItemStack(Material.CAVE_AIR, 5));
+        }
+    }
+
+    @EventHandler
+    public void onCraftTemplate(PrepareItemCraftEvent e){
+        CraftingInventory cInv = e.getInventory();
+        if (cInv.contains(Material.NETHERITE_UPGRADE_SMITHING_TEMPLATE)){
+         cInv.setResult(new ItemStack(Material.CAVE_AIR, 2));
         }
     }
 
